029158f8f79d425d5dbf1013ef5e2019f9d42beb
Fulton Garcia <FultonG@users.noreply.github.com>
2019-03-08 09:32:46 -0500

    readding proxy

--- src/utils/CatFacts.js
+++ src/utils/CatFacts.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export default {
     getFacts: function(){
-        //if running locally use https://thingproxy.freeboard.io/fetch/ before the url
-        return axios.get("https://cat-fact.herokuapp.com/facts");
+        return axios.get("https://thingproxy.freeboard.io/fetch/https://cat-fact.herokuapp.com/facts");
     }
 }
